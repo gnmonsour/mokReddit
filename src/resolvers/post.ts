184147cd2ac24c7bbff3d7a165fa4b476d21bc23
George N Monsour <gnmonsour@gmail.com>
2021-03-16 18:11:24 -0400

    Provide a create query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,19 +1,29 @@
 import { Post } from '../entities/Post';
 import { ConnContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em }: ConnContext): Promise<Post[]> {
+  getAllPosts(@Ctx() { em }: ConnContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
   @Query(() => Post, { nullable: true })
-  post(
+  getPost(
     @Arg('id', () => Int) id: number,
     @Ctx() { em }: ConnContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: ConnContext
+  ): Promise<Post> {
+    const newPost = em.create(Post, { title });
+    await em.persistAndFlush(newPost);
+    return newPost;
   }
 }
